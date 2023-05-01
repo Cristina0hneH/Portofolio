f46d7d28cc37ba3e9183eb45498927057244e68c
Cristina0hneH <cristinacaran@Cristinas-MacBook-Air.fritz.box>
2023-05-01 23:59:24 +0200

    styled hero svg in light mode

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,1 +1,21 @@
-<h1>Bro</h1>
+import '../hero/hero__light.css';
+import CvBtn from '../downloadBtn/CvBtn';
+import LogoLight from '../../assets/logo/Logo__light';
+import LogoDark from '../../assets/logo/Logo__dark';
+
+export default function Hero(){
+    return <div className="hero">
+        <section>
+        <h1>Hello World!</h1>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        <div>
+            <CvBtn />
+            <button className="contactBtn">Contact</button>
+        </div>
+        </section>
+        <aside>
+            {/*<LogoDark />*/}
+            <LogoLight />
+        </aside>
+    </div>
+}
