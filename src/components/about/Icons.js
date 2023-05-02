d27bf21ebed94726d332bd35ec8916b3333e06be
Cristina0hneH <cristinacaran@Cristinas-Air.fritz.box>
2023-05-02 19:57:15 +0200

    oops, forgot to make a new branch, so I'll merge now, while the code works lol

---
+++ src/components/about/Icons.js
@@ -0,0 +1,14 @@
+
+export default function(){
+    return <div className="icons">
+            <i class="devicon-html5-plain-wordmark colored"></i>
+            <i class="devicon-css3-plain-wordmark colored"></i>
+            <i class="devicon-bootstrap-plain colored"></i>
+            <i class="devicon-materialui-plain colored"></i>
+            <i class="devicon-javascript-plain colored"></i>
+            <i class="devicon-react-original-wordmark colored"></i>
+            <i class="devicon-nodejs-plain colored"></i>  
+            <i class="devicon-mongodb-plain-wordmark colored"></i>
+                    
+    </div>
+}
